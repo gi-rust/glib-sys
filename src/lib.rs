569c4953dd9d724c06c813b5667ac39d76c33bf4
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2015-01-24 19:44:16 +0200

    Spun off glib FFI definitions as a Cargo project
    This follows the Cargo policy of one -sys crate per native library.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
-// This file is part of Grust, GObject introspection bindings for Rust
-//
-// Copyright (C) 2013, 2014  Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
+// Copyright (C) 2013-2015  Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
@@ -16,30 +14,50 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-#![crate_name = "grust"]
+#![crate_name = "glib-2_0-sys"]
 #![crate_type = "lib"]
 
+#![allow(missing_copy_implementations)]
 #![allow(unstable)]
-#![allow(unstable_features)]
-
-#![feature(unsafe_destructor)]
 
 extern crate libc;
 
-pub mod error;
-pub mod gstr;
-pub mod gtype;
-pub mod mainloop;
-pub mod marker;
-pub mod object;
-pub mod quark;
-pub mod refcount;
 pub mod types;
-pub mod util;
-pub mod value;
-pub mod wrap;
 
-mod ffi;
+use types::{gboolean, gchar, gint, gpointer};
 
-#[macro_use]
-mod macros;
+#[repr(C)]
+pub struct GError {
+    pub domain: GQuark,
+    pub code: gint,
+    pub message: *const gchar
+}
+
+#[repr(C)]
+pub struct GMainContext;
+
+#[repr(C)]
+pub struct GMainLoop;
+
+pub type GQuark = u32;
+
+extern {
+    pub fn g_free(mem: gpointer);
+    pub fn g_error_copy(error: *const GError) -> *mut GError;
+    pub fn g_error_free(error: *mut GError);
+    pub fn g_main_context_new() -> *mut GMainContext;
+    pub fn g_main_context_ref(context: *mut GMainContext) -> *mut GMainContext;
+    pub fn g_main_context_unref(context: *mut GMainContext);
+    pub fn g_main_context_default() -> *mut GMainContext;
+    pub fn g_main_context_push_thread_default(context: *mut GMainContext);
+    pub fn g_main_context_pop_thread_default(context: *mut GMainContext);
+    pub fn g_main_loop_new(ctx: *mut GMainContext, is_running: gboolean) -> *mut GMainLoop;
+    pub fn g_main_loop_ref(l: *mut GMainLoop) -> *mut GMainLoop;
+    pub fn g_main_loop_unref(l: *mut GMainLoop);
+    pub fn g_main_loop_get_context(l: *mut GMainLoop) -> *mut GMainContext; 
+    pub fn g_main_loop_run(l: *mut GMainLoop);
+    pub fn g_main_loop_quit(l: *mut GMainLoop);
+    pub fn g_quark_from_static_string(string: *const gchar) -> GQuark;
+    pub fn g_quark_to_string(quark: GQuark) -> *const gchar;
+    pub fn g_strdup(str: *const gchar) -> *mut gchar;
+}
