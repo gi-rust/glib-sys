a232f582db2c26cdadeb5928679eda2caf808c91
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2015-06-11 11:15:21 +0300

    Add GSource and some of the related functions

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,7 @@ extern crate libc;
 
 pub mod types;
 
-use types::{gboolean, gchar, gint, gpointer};
+use types::{gboolean, gchar, gint, gpointer, guint};
 
 #[repr(C)]
 pub struct GError {
@@ -37,6 +37,9 @@ pub struct GMainContext;
 
 #[repr(C)]
 pub struct GMainLoop;
+
+#[repr(C)]
+pub struct GSource;
 
 pub type GQuark = u32;
 
@@ -61,6 +64,7 @@ extern {
     pub fn g_error_copy(error: *const GError) -> *mut GError;
     pub fn g_error_free(error: *mut GError);
     pub fn g_error_new_literal(domain: GQuark, code: gint, message: *const gchar) -> *mut GError;
+    pub fn g_idle_source_new() -> *mut GSource;
     pub fn g_main_context_new() -> *mut GMainContext;
     pub fn g_main_context_ref(context: *mut GMainContext) -> *mut GMainContext;
     pub fn g_main_context_unref(context: *mut GMainContext);
@@ -76,5 +80,13 @@ extern {
     pub fn g_main_loop_quit(l: *mut GMainLoop);
     pub fn g_quark_from_static_string(string: *const gchar) -> GQuark;
     pub fn g_quark_to_string(quark: GQuark) -> *const gchar;
+    pub fn g_source_ref(source: *mut GSource) -> *mut GSource;
+    pub fn g_source_unref(source: *mut GSource);
+    pub fn g_source_attach(source: *mut GSource, context: *mut GMainContext) -> guint;
+    pub fn g_source_destroy(source: *mut GSource);
+    pub fn g_source_set_callback(source: *mut GSource, func: GSourceFunc, data: gpointer, notify: Option<GDestroyNotify>);
+    pub fn g_source_set_priority(source: *mut GSource, priority: gint);
     pub fn g_strdup(str: *const gchar) -> *mut gchar;
+    pub fn g_timeout_source_new(interval: guint) -> *mut GSource;
+    pub fn g_timeout_source_new_seconds(interval: guint) -> *mut GSource;
 }
